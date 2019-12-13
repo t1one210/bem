db26c2784d3d44c62738356538842bd186e80fe3
t1one210 <korepanov.andrew@mail.ru>
2019-12-13 13:39:54 +0400

    corrections

--- taskJS/js.js
+++ taskJS/js.js
@@ -1,13 +1,11 @@
 
 
 function handleFileSelect(event) {
-    var files = event.target.files; // FileList object
+    var files = event.target.files;
+    var len = files.length; 
+    for (var i = 0; i < len; i++) {
 
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-      // Only process image files.
-      if (!f.type.match('image.*')) {
+      if (!files[i].type.match('image.*')) {
         continue;
       }
 
@@ -18,62 +16,142 @@ function handleFileSelect(event) {
         img.src = reader.result
         document.getElementById('list').appendChild(img)
       }
-      // Closure to capture the file information.
       reader.onload = (function(theFile) {
         return function(e) {
-          // Render thumbnail.
-          var span = document.createElement('span');
-          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+          var li = document.createElement('li');
+          li.innerHTML = ['<img class="thumb" src="', e.target.result,
                             '" title="', escape(theFile.name), '"/>'].join('');
-          document.getElementById('list').prepend(span);
-        
+          document.getElementById('list').prepend(li);
         };
-      })(f);
-
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
+        
+      })(files[i]);
+      
+      reader.readAsDataURL(files[i]);
     }
-  }
+}
   window.onload=function(){
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  document.getElementById('fils').addEventListener('change', handleFileSelect, false);
 }
 
 function allowDrop(event) {
   event.preventDefault();
 }
 
-function drag(event) {
-  event.dataTransfer.setData("text/plain", event.currentTarget.id);
-}
-
 function drop(event) {
   event.preventDefault();
-  var data = document.createElement('span');
-  data.innerHTML = ['<img class="thumb" src="', event.target.result,
-  '"/>'].join('');
-  document.getElementById('list').prepend(data);
-}
 
-/*var target = document.getElementById("list")
-  target.addEventListener("dragover", function(event) {
-    event.preventDefault(); // отменяем действие по умолчанию
-  }, false);
-  target.addEventListener("drop", function(event) {
-        // отменяем действие по умолчанию
-    event.preventDefault();
-    event.stopPropagation();
-      var i = 0,
-      files = event.dataTransfer.files,
-      len = files.length;
-      for (; i < len; i++) {
-          console.log("Filename: " + files[i].name);
-          console.log("Type: " + files[i].type);
-          console.log("Size: " + files[i].size + " bytes");
-      }
-  }, false);*/
+  var files = event.dataTransfer.files; 
+  var len = files.length; 
+    for (var i = 0; i < len; i++) {
+
+      var reader = new FileReader();
+      reader.onload = (function(File) {
+        return function(e) {
+          var li = document.createElement('li');
+          li.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(File.name), '"/>'].join('');
+          document.getElementById('list').prepend(li);
+      };
+        
+      })(files[i]);
+
+      reader.readAsDataURL(files[i]);
+    }    
+}
 
   $(document).ready(function(){
     $("#list").sortable();
  });
-    
-    
+ 
+var promise = new Promise(function(resolve,reject){
+  var n = function () {
+    var sum;
+    sum++;
+    resolve (sum);
+    //console.log (sum);
+  };
+
+  () => {
+    for (let i = 0; i< 3; i++)
+    i = 8;
+    reject(new Error ("error"));
+    //console.log (i);
+  }
+})
+
+promise.then(function(value) {
+  console.log(value);
+})
+//console.log(promise.then(1))
+console.log(error => promise.catch('Error: ' + error))
+
+var promise1 = new Promise (function(resolve, reject) {
+  setTimeout(function() {
+    resolve('foo');
+  }, 300);
+
+  setTimeout(function (){
+    resolve('nyobo nyobo');
+  },1);
+});
+
+promise1.then(function(value) {
+  console.log(value);
+  // expected output: "foo"
+});
+
+console.log(promise1);
+// expected output: [object Promise]
+
+var array = [0,1,2,3];
+var vvod = parseInt(prompt('vvedite znachenie', ''));
+console.log(vvod);
+console.log(array.includes(vvod));
+  for (let i = 0; i<=array.length; i++){
+    if (array.includes(vvod) == i || typeof isNaN(vvod)){
+    switch (i){
+      case 0:
+        console.log('index = 0');
+        break;
+      case 1:
+        console.log('index = 1')
+        break;
+      case 3:
+        console.log('index = 4')
+        break;
+      default:
+        console.log("isn't number or not found");
+    }
+  } 
+} 
+
+function greeting(name) {
+  console.log('Hello ' + name);
+}
+
+function processUserInput(collback) {
+  var name = prompt('Please enter your name.');
+  collback(name);
+}
+
+processUserInput(greeting);
+
+
+
+
+Array.prototype.partition = function(pred) {
+  let passed = []
+  let passed2 = []
+  let failed = []
+  for(let i = 0; i < this.length; i++) {
+    if (pred(this[i])) {
+      passed.push(this[i])
+      passed2.push(this[i])
+    } else {
+     failed.push(this[i])
+   }
+  }
+  return [ passed, failed, passed2 ]
+}
+
+console.log([1,2,3,4,56,7].partition (e => e <= 2));
