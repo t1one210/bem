cb6a801a03f9dad7784bfaa6943d1a1ad9bf6179
t1one210 <korepanov.andrew@mail.ru>
2019-12-19 10:48:02 +0400

    correction syntax oop

--- oop/Самолет.js
+++ oop/Самолет.js
@@ -3,27 +3,21 @@ class TY {
         this.x=x;
         this.y=y;
     }
-    moveTo(x,y);
-    landingTo(x,y);
-}
-
-class MIG {
-    constructor(x,y){
-        this.x=x;
-        this.y=y
-    }
-    fireTo(x,y);
+    moveTo() {}
 }
 
 class MIG extends TY{
 
-    fireTo();
+    fireTo() {}
 }
 
-var ty = new TY();
-ty.moveTo(10,10);
-ty.landingTo(0,0);
-var mig = new MIG();
-mig.moveTo(10,10);
-mig.fireTo(15,15);
-mig.landingTo(0,0);
+var ty = new TY(0,0);
+ty.moveTo(); //взлет
+ty.moveTo(10,10); //полет
+ty.moveTo(); //посадка
+
+var mig = new MIG(15,15);
+mig.moveTo(0,0); //взлет
+mig.moveTo(10,10); //полет
+mig.moveTo(0,0); //посадка
+mig.fireTo();
