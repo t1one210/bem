c6a7b5a94ed45fdee814c5eb4a408604980b48c3
t1one210 <korepanov.andrew@mail.ru>
2019-12-26 14:26:49 +0400

    taskJS add progress

--- taskJS/js.js
+++ taskJS/js.js
@@ -1,62 +1,72 @@
-function handleFileSelect(event) {
-    var files = event.target.files;
-    var len = files.length; 
-    for (var i = 0; i < len; i++) {
+function Select(event) {
+  var files = event.target.files;
+  handleFile(files);
+}
 
-      if (!files[i].type.match('image.*')) {
-        continue;
-      }
+function drop(event) {
+  event.preventDefault();
+  var files = event.dataTransfer.files; 
+  handleFile(files);
+}
 
-      var reader = new FileReader();
+  document.getElementById('fils').addEventListener('change', Select, false);
+  document.getElementById('target').addEventListener('drop', drop, false);
 
-      reader.onloadend = function() {
-        let img = document.createElement('img')
-        img.src = reader.result
-        document.getElementById('list').appendChild(img)
-      }
+function handleFile(files){
+  var len = files.length;
+  for (var i = 0; i < len; i++) {
+
+    if (!files[i].type.match('image.*')) {
+      continue;
+    }
+
+    var reader = new FileReader();
+    if (files.length < 12){
       reader.onload = (function(theFile) {
         return function(e) {
+          let img = document.createElement('img');
+          img.src = reader.result;
           var li = document.createElement('li');
           li.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-          document.getElementById('list').prepend(li);
+            '" title="', escape(theFile.name), '"/>'].join('');
+            document.getElementById('list').prepend(li);
+            move(files);
         };
-        
       })(files[i]);
-      
-      reader.readAsDataURL(files[i]);
     }
-}
-  window.onload=function(){
-  document.getElementById('fils').addEventListener('change', handleFileSelect, false);
+      
+      var sum = document.getElementsByTagName('li');
+      if (sum.length > 13){
+        alert ("Nyobo Nyobo");
+        //reader.onabort;
+        break;
+      }
+    reader.readAsDataURL(files[i]);
+  }
 }
 
 function allowDrop(event) {
   event.preventDefault();
 }
 
-function drop(event) {
-  event.preventDefault();
+$(document).ready(function(){
+  $("#list").sortable();
+});
 
-  var files = event.dataTransfer.files; 
-  var len = files.length; 
-    for (var i = 0; i < len; i++) {
-
-      var reader = new FileReader();
-      reader.onload = (function(File) {
-        return function(e) {
-          var li = document.createElement('li');
-          li.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(File.name), '"/>'].join('');
-          document.getElementById('list').prepend(li);
-      };
-        
-      })(files[i]);
-
-      reader.readAsDataURL(files[i]);
-    }    
-}
-
-  $(document).ready(function(){
-    $("#list").sortable();
- });
+function move(files) {
+  var elem = document.getElementById("greenBar");
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {    
+    for (let i = 0; i <= files.length; i++){
+        if (width >= 100) {
+          clearInterval(id);
+        } else{
+            width++;
+            //elem.style.width = (files.length) / files.length * 100 + '%'; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = Math.round(files.length / files.length * 100) + '%';
+          }
+    }       
+  }  
+} 
